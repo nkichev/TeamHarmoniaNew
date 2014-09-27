3ec7a10807f25e48ced3c9cfa988a383a1fa210d
Nikolay Kichev <kichev.nikolay@gmail.com>
2014-09-27 14:57:23 -0500

    Changes made by Nikolay Kichev (kichev.nikolay@gmail.com).

---
+++ Postcard Sender App/scripts/view-models/postcards.js
@@ -0,0 +1,24 @@
+var app = app || {};
+app.viewmodels = app.viewmodels || {};
+
+(function (scope) {
+    function loadPostcards(){
+        var postcards = [];
+        window.everlive.data('Postcard').get().then(function(data){
+            data.result.forEach(function(postcard){
+                postcards.push({
+                    title: postcard.Title
+                });
+            })
+            return postcards;
+        });
+    };
+    
+    
+    scope.postcards = function(e){
+        var vm = kendo.observable({
+            postcards: loadPostcards()
+        });
+        kendo.bind(e.view.element, vm);
+    };    
+}(app.viewmodels));
---
+++ Postcard Sender App/scripts/view-models/sendPostcard.js
@@ -0,0 +1,6 @@
+var app = app || {};
+app.viewmodels = app.viewmodels || {};
+
+(function(scope){
+        
+}(app.viewmodels));
