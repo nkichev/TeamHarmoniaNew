9f277e7613b42bde6dd1d18cc2500d19b9a04610
Nikolay Kichev <kichev.nikolay@gmail.com>
2014-09-28 07:22:46 -0500

    Changes made by Nikolay Kichev (kichev.nikolay@gmail.com).

--- Postcard Sender App/scripts/view-models/postcards.js
+++ Postcard Sender App/scripts/view-models/postcards.js
@@ -2,24 +2,34 @@ var app = app || {};
 app.viewmodels = app.viewmodels || {};
 
 (function (scope) {
-    function loadPostcards(){
+
+    function loadPostcards() {
         var postcards = [];
-        window.everlive.data('Postcard').get().then(function(data){
-            data.result.forEach(function(postcard){
-                postcards.push({
-                    title: postcard.Title
-                });
-            })
-            return postcards;
-        });
+        scope.postcards = kendo.observable({
+                postcards: postcards
+            });
+        return window.everlive.data('Postcard')
+            .get()
+            .then(function (data) {
+                data.result.forEach(function (postcard) {
+                    $.ajax({
+                        type: "GET",
+                        url: 'http://api.everlive.com/v1/P7b0o35jcf7ft5Uk/Files/' + postcard.Pic,
+                        contentType: "application/json"
+                    }).then(function (picData) {
+                        scope.postcards.get("postcards").push({
+                            imageSrc: picData.Result.Uri,
+                            content: postcard.Content,
+                            receiver: postcard.Receiver
+                        });
+                    })
+                })
+                
+            });
+        
     };
-    
-    
-    
-    scope.postcards = function(e){
-        var vm = kendo.observable({
-            postcards: loadPostcards()
-        });
-        kendo.bind(e.view.element, vm);
-    };   
+
+
+    loadPostcards();
+
 }(app.viewmodels));
--- Postcard Sender App/scripts/view-models/sendPostcard.js
+++ Postcard Sender App/scripts/view-models/sendPostcard.js
@@ -1,6 +1,64 @@
 var app = app || {};
 app.viewmodels = app.viewmodels || {};
 
-(function(scope){
-        
-}(app.viewmodels));
+(function (scope) {
+        /* scope.sendPostcard = kendo.observable({
+        title: '',
+        save: function(ev){
+            window.everlive.data('Postcard').create({
+                Title: this.get('title')
+            });
+        }
+    }); */
+
+        scope.sendPostcard = kendo.observable({
+                save: function () {
+                    var location = {};
+                    var content = this.get('content');
+                    var receiver = this.get('receiver');
+                    var picSuccess = function (data) {
+                        var id;
+                        window.everlive.Files.create({
+                                Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                                ContentType: "image/jpeg",
+                                base64: data
+                            },
+                            function (picData) {
+                                //getgeo
+                                window.everlive.data('Postcard').create({
+                                    'Pic' : picData.result.Id,
+                                    'Location': location,
+                                    'Content': content,
+                                    'Receiver': receiver
+                                }, function(data) {
+                                    console.log(data);
+                                }, error);
+                            }, error);
+                        };
+                        var error = function () {
+                            navigator.notification.alert("Unfortunately we could not add the image");
+                        };
+                        var picConfig = {
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            targetHeight: 400,
+                            targetWidth: 400
+                        };
+                    
+                    var geoConfig = {
+                        enableHighAccuracy : true  
+                    };
+                    var geoSuccess = function(data){
+                        location = {
+                            longitude: data.coords.longitude,
+                            latitude: data.coords.latitude
+                        };
+                        
+                        navigator.camera.getPicture(picSuccess, error, picConfig);
+                    }
+
+                        navigator.geolocation.getCurrentPosition(geoSuccess, error, geoConfig);
+                    }
+                });
+
+
+        }(app.viewmodels));
